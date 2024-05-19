8efb9cc54e99958ad8772d4518bcf867f80851f8
BH3Panzer <clementcattin74@proton.me>
2024-05-19 23:24:03 +0200

    windows support for screen

--- screen.go
+++ screen.go
@@ -2,9 +2,21 @@ package main
 
 import "os"
 import "os/exec"
+import "runtime"
+
+func getRightCommand() string {
+	if runtime.GOOS == "windows" {
+		return "cls"
+	} else if runtime.GOOS == "linux" {
+		return "clear"
+	} else {
+		return "clear"
+	}
+}
 
 func Clear() {
-	clear := exec.Command("clear")
+	com := getRightCommand()
+	clear := exec.Command(com)
 	clear.Stdout = os.Stdout
 	clear.Run()
 }
