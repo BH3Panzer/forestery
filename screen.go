7131911546e4eccb0820284f07b6e75ae6914a1c
BH3Panzer <clementcattin74@proton.me>
2024-05-20 00:39:39 +0200

    finally fixed

--- screen.go
+++ screen.go
@@ -4,19 +4,14 @@ import "os"
 import "os/exec"
 import "runtime"
 
-func getRightCommand() string {
-	if runtime.GOOS == "windows" {
-		return "cmd /c cls"
-	} else if runtime.GOOS == "linux" {
-		return "clear"
-	} else {
-		return "clear"
-	}
-}
-
 func Clear() {
-	com := getRightCommand()
-	clear := exec.Command(com)
-	clear.Stdout = os.Stdout
-	clear.Run()
+	if runtime.GOOS == "windows" {
+		clear := exec.Command("cmd", "/c", "cls")
+		clear.Stdout = os.Stdout
+		clear.Run()
+	} else if runtime.GOOS == "linux" {
+		clear := exec.Command("clear")
+		clear.Stdout = os.Stdout
+		clear.Run()
+	}
 }
